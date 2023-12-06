319b0755b10c8d3c2b68529da5eb5aac70095bda
ocaspary <44494044+ocaspary@users.noreply.github.com>
2023-12-06 22:09:41 +0100

    Add files via upload

---
+++ accessingDataJPAwithREST/src/main/java/com/example/accessingdatarest/AccessingDataJpAwithRestApplication.java
@@ -0,0 +1,15 @@
+// https://spring.io/guides/gs/accessing-data-rest/ 
+
+package com.example.accessingdatarest;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AccessingDataJpAwithRestApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AccessingDataJpAwithRestApplication.class, args);
+	}
+
+}
